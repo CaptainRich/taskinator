e056cf85cacbb1a24f2b8483965e0972269392fa
RichA <rich_ay@sbcglobal.net>
2020-08-15 09:30:56 -0500

    Read form (task) input and display it on the page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,11 +13,24 @@ var createTaskHandler = function( event ) {
 
     event.preventDefault();                            /* prevent the browser from reloading the page. */
 
+    /* Obtain the task name and type just defined from the input form. */
+    var taskNameInput = document.querySelector( "input[name='task-name']").value;
+    var taskTypeInput = document.querySelector( "select[name='task-type']").value;
+    
+
     var listItemEl = document.createElement( "li" );   /* create the "li" item/selector. */
     listItemEl.className = "task-item";                /* assign the proper class to this new item. */
-    listItemEl.textContent = "This is a new task." ;   /* add the text content to the item. */
+
+    /* Create a 'div' to hold the task info and add it to the list item just created. */
+    var taskInfoEl = document.createElement( "div" );
+    taskInfoEl.className = "task-info";                /* give the 'div' a class name
+
+    /* Add content and style to this new 'div' element */
+    taskInfoEl.innerHTML = "<h3 class-'task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    /* Put things all together using .appendChild */
+    listItemEl.appendChild( taskInfoEl );              /* this adds the 'h3' and 'span' data */
     tasksToDoEl.appendChild( listItemEl );             /* add the new "li"  to its parent, the "ul" item */   
-    console.log( event );
 }
 
 /* Setup the (form) event handler and call-back function .  When the form is submitted the handler will create a new task. The "submit" event is invoked when a button with 'type=submit' is clicked, or the user presses '[Enter]'. */
